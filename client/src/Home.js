ea6ece8c243e0900178be2286f0a21b8981a7a7f
Scott Molsness <smolsness@gmail.com>
2018-03-19 11:54:27 -0700

    added Materialize and basic structure to Home component

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 export const Home = props => {
   return (
-    <div>
-      <p>Home!</p>
-
+    <div className='row'>
+      <div className='row'>
+        <div className='col s2'></div>
+        <div className='col s8'>
+          <h1>Apocalyprep Home</h1>
+        </div>
+        <div className='col s2'></div>
+      </div>
+      <div className='row'>
+        <form className='col s12'>
+          <div className='col s3'></div>
+          <div className='input-field col s6'>
+            <input id="zip" type="text" className="validate" />
+            <label for="zip">Enter your Zip Code</label>
+          </div>
+          <div className='col s3'></div>
+        </form>
+      </div>
     </div>
   )
 }
