a9c41ae136e8bf256ae6044c9cc67773bd3fdb65
hannan-emily <emilyhannan@gmail.com>
2018-03-21 14:45:23 -0700

    new

--- client/src/SupplyList.js
+++ client/src/SupplyList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import axios from 'axios';
-import {Collapsible, Icon} from 'react-materialize'
+import { Collapsible } from 'react-materialize'
 
 
 
