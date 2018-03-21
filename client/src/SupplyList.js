f1ef28c07acc4177b6c99fee41e42a316741072d
hannan-emily <emilyhannan@gmail.com>
2018-03-21 14:28:31 -0700

    got the supply list link from the database through to react working. now the supply list component loads the supply data, filters it by category and returns a filtered list of items

--- client/src/SupplyList.js
+++ client/src/SupplyList.js
@@ -20,87 +20,84 @@ class SupplyList extends Component  {
     }
   }
 
+  filterSupply = () => {
+    // filter all supplies into categories & new arrays using swich case
+    this.state.allSupply.forEach((item) => {
+      switch(true) {
+        case item.category === "Medical":
+          this.setState({
+            medicalArray: [...this.state.medicalArray, item.name]
+          })
+          break;
+        case item.category === "Food and Water":
+          this.setState({
+            foodwaterArray: [...this.state.foodwaterArray, item.name]
+          })
+          break;
+        case item.category === "Tools and Supplies":
+          this.setState({
+            toolArray: [...this.state.toolArray, item.name]
+          })
+          break;
+        case item.category === "Tech":
+          this.setState({
+            techArray: [...this.state.techArray, item.name]
+          })
+          break;
+        case item.category === "Documents":
+          this.setState({
+            docArray: [...this.state.docArray, item.name]
+          })
+          break;
+        default:
+          return 'foo';
+      }
+    }), () => console.log(this.state.foodwaterArray)
+  }
+
   componentDidMount() {
     //call database using Axios. this route is set up in server.js
     //put all things in 1 big array -- allSupply[].
     //this call runs only once.
     axios.get('/api/supplylist').then(result => {
-    this.setState({
-      //we're getting the data sent from server.js
-      allSupply: result.data
-    }, () => console.log(this.state.allSupply))
+      this.setState({
+        //we're getting the data sent from server.js
+        allSupply: result.data
+      })
+    }).then(() => {
+      console.log('hihi');
+      this.filterSupply()
+      console.log(this.state)
+
     })
-
-
-    // filter using swich case into 4 new arrays
-  //   filterSupply(allSupply) {
-  //     allSupply.forEach(function(item) {
-  //       switch(true) {
-  //         case item.category = "Medical":
-  //           medicalArray.push(item.name)
-  //           break;
-  //         case item.category = "Food and Water":
-  //           foodwaterArray.push(item.name)
-  //           break;
-  //         case item.category = "Tools and Supplies":
-  //           toolArray.push(item.name)
-  //           break;
-  //         case item.category = "Tech":
-  //           techArray.push(item.name)
-  //           break;
-  //         case item.category = "Documents":
-  //           docArray.push(item.name)
-  //           break;
-  //         default:
-  //           return 'foo';
-  //       }
-  //     })
-  //
-  //   }
-  //
   }
 
   render() {
-  {/*  //map through the 4 arrays i've built to create li elements
-     // var medicalList = medicalArray.map((item,index) => <li key={index}>{item}</li>)
-     // var foodwaterList = foodwaterArray.map((item,index) => <li key={index}>{item}</li>)
-     // var toolList = toolArray.map((item,index) => <li key={index}>{item}</li>)
-     // var techList = techArray.map((item,index) => <li key={index}>{item}</li>)
-     // var docList = docArray.map((item,index) => <li key={index}>{item}</li>)
-*/}
+     // map through the 4 arrays i've built to create li elements
+     var medicalList = this.state.medicalArray.map((item,index) => <li key={index}>{item}</li>)
+     var foodwaterList = this.state.foodwaterArray.map((item,index) => <li key={index}>{item}</li>)
+     var toolList = this.state.toolArray.map((item,index) => <li key={index}>{item}</li>)
+     var techList = this.state.techArray.map((item,index) => <li key={index}>{item}</li>)
+     var docList = this.state.docArray.map((item,index) => <li key={index}>{item}</li>)
 
 
       return (
         <div>
-          <Row>
-          {/* // <Collapsible accordion>
-          //
-          //   <CollapsibleItem header='Medical' icon='filter_drama'>
-          //
-          //   </CollapsibleItem>
-          //
-          //   <CollapsibleItem header='Food & Water' icon='place'>
-          //
-          //   </CollapsibleItem>
-          //
-          //   <CollapsibleItem header='Tools & Supplies' icon='whatshot'>
-          //
-          //   </CollapsibleItem>
-          //
-          //   <CollapsibleItem header='Tech' icon='whatshot'>
-          //
-          //   </CollapsibleItem>
-          //
-          //   <CollapsibleItem header='Documents' icon='whatshot'>
-          //
-          //   </CollapsibleItem>
-          //
-           // </Collapsible> */}
-          </Row>
+        <h2> Medical</h2>
+          {medicalList}
+          <h2> Food Water</h2>
+            {foodwaterList}
+        <h2> Tool List</h2>
+          {toolList}
+          <h2> Tech </h2>
+            {techList}
+            <h2> Documents </h2>
+              {docList}
         </div>
       )
-    }
+
   }
+}
 
 
 export default SupplyList;
