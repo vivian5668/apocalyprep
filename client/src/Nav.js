87e3d2c0c2c7ac75cf0ff7423611442d1939216c
hannan-emily <emilyhannan@gmail.com>
2018-03-23 16:53:02 -0700

    refactored and styled nav bar with valid routes

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Navbar, NavItem, Icon, Dropdown } from 'react-materialize';
+import { Navbar, NavItem, Icon, Dropdown, Button } from 'react-materialize';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Nav = props => {
@@ -10,10 +10,21 @@ const Nav = props => {
     return (
       <Navbar brand='apocalyprep' right id='navbar'>
         <NavItem><Link to="/">Home</Link></NavItem>
-        <NavItem><Link to="/location">Disasters</Link></NavItem>
-        <NavItem><Link to="/ImageAccordion">Alerts</Link></NavItem>
-        <NavItem><Link to="/supplylist">Supplies</Link></NavItem>
+        <NavItem><Link to="/ImageAccordion">Disasters</Link></NavItem>
+
+        <NavItem><Dropdown className="dropdown-button" trigger={
+          <Button>Single Disasters:</Button>
+          }>
+          <NavItem><Link to="/tsunami">Tsunami</Link></NavItem>
+          <NavItem><Link to="/hurricane">Hurricane</Link></NavItem>
+          <NavItem><Link to="/tornado">Tornado</Link></NavItem>
+          <NavItem><Link to="/earthquake">Earthquake</Link></NavItem>
+          <NavItem><Link to="/wildfire">Wildfire</Link></NavItem>
+
+        </Dropdown></NavItem>
+
         <NavItem><Link to="/about">About</Link></NavItem>
+
         <li>
           <Dropdown trigger={<a><Icon>person</Icon></a>}>
             <NavItem>Hello, {props.user.name}!</NavItem>
@@ -28,11 +39,21 @@ const Nav = props => {
     )
   } else if (props.user === null){
     return (
-      <Navbar brand='apocalyprep' right id='navbar'>
+        <Navbar brand='apocalyprep' right id='navbar'>
         <NavItem><Link to="/">Home</Link></NavItem>
-        <NavItem><Link to="/location">Disasters</Link></NavItem>
-        <NavItem><Link to="/ImageAccordion">Alerts</Link></NavItem>
-        <NavItem><Link to="/supplylist">Supplies</Link></NavItem>
+        <NavItem><Link to="/ImageAccordion">Disasters</Link></NavItem>
+
+        <NavItem><Dropdown className="dropdown-button" trigger={
+          <Button>Single Disasters:</Button>
+          }>
+          <NavItem><Link to="/tsunami">Tsunami</Link></NavItem>
+          <NavItem><Link to="/hurricane">Hurricane</Link></NavItem>
+          <NavItem><Link to="/tornado">Tornado</Link></NavItem>
+          <NavItem><Link to="/earthquake">Earthquake</Link></NavItem>
+          <NavItem><Link to="/wildfire">Wildfire</Link></NavItem>
+
+        </Dropdown></NavItem>
+
         <NavItem><Link to="/about">About</Link></NavItem>
         <NavItem><Link to="/login">Login</Link></NavItem>
         <NavItem><Link to="/signup">Signup</Link></NavItem>
