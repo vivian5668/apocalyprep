3d8db76bc1c0273af4a7ae8bbb7c3f89c953a8a5
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-03-23 13:34:32 -0700

    style'n

--- client/src/Alerts.js
+++ client/src/Alerts.js
@@ -20,40 +20,23 @@ const Alerts = (props) => {
     console.log("in alerts:", props.alerts)
     if (props.alerts === null) {
       return(
-        <Row id="videoRow">
-          <Col s={8}>
-        		<LocationEnter />
-            <h6>Error, please re-enter city and state </h6>
-          </Col>
-      	</Row>
+        <div className="yourMom">
+      		<LocationEnter />
+          <h6>Error, please re-enter city and state </h6>
+        </div>
 
       )
     } else {
       return(
 
-          <div>
+          <div className="yourMom">
             <h3>alerts</h3>
             {
               props.alerts.map( (data, index) => {
                 if (data.properties.severity === 'Severe') {
                   return (
                     <div key={index}>
-                      {
-                        // <button
-                        //   data-alert={data.properties.headline}
-                        //   data-area={data.properties.areaDesc}
-                        //   data-instruct={data.properties.instruction}
-                        //   data-response={data.properties.response}
-                        // >
-                        //   {data.properties.headline}
-                        // </button>
-                      // <p>Alert: {data.properties.headline}</p>
-                      // <p>Area: {data.properties.areaDesc}</p>
-                      // <p>Description: {data.properties.description}</p>
-                      // <p>Instruction: {data.properties.instruction}</p>
-                      // <p>Response: {data.properties.response}</p>
-                      }
-                      <Collapsible className='black-text'>
+                      <Collapsible className='yourDad black-text'>
                         <CollapsibleItem header={data.properties.headline} icon='warning'>
                           <p className='white-text'>Area:<br />{data.properties.areaDesc}</p>
                           <p className='white-text'>Description:<br />{data.properties.description}</p>
